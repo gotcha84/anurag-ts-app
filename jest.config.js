d12447c07865496a095d9434cd1cd239ca256154
gotcha84 <anurag.kalavakunta@hingehealth.com>
2024-10-09 17:02:53 -0700

    Add default ts-jest preset

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,23 @@ const { defaults: tsjPreset } = require("ts-jest/presets");
 
 /** @type {import('jest').Config} */
 module.exports = {
+  ...tsjPreset,
   verbose: true,
   preset: 'react-native',
   setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  transform: {
+    "^.+\\.(js|jsx|mjs)$": "babel-jest",
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        isolatedModules: true,
+        /**
+         * Extend `tsconfig.json` to use `react` jsx instead of `react-native` jsx for ts-jest
+         * See ts-jest setup for react-native: https://kulshekhar.github.io/ts-jest/docs/guides/react-native#typescript-configuration
+         */
+        tsconfig: "tsconfig.spec.json",
+      },
+    ],
+  },
   transformIgnorePatterns: [],
 };
